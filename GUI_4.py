9d1b2f16b102a7e75e53a2f06e77687d9482502a
Stephen Summering <summeringpainting@gmail.com>
2019-12-12 15:38:13 -0500

    closer

--- GUI_4.py
+++ GUI_4.py
@@ -1,6 +1,5 @@
 from tkinter import *
 import random
-
 guessRange = "guessRange"
 root = Tk()
 
@@ -9,16 +8,40 @@ e.pack(pady=20)
 e.focus_set()
 
 class coastButtons():
-    def printtext(self):
+    def printtext(self, counter=0, counter1=1, luckvar=3, countup=2, level=1):
         global e
-        string = e.get()
-        print(string)
+        string = e.get
+        string = int(e.get())
+        loto = string
+        luck = random.randint(1, luckvar)
+        while luck != loto:
+
+            if loto <= (luck + 3) and loto >= (luck - 3):
+                counter += 1
+                if counter > counter1:
+                    print("too many guesses")
+                    break
+                else:
+                    print("closey")
+                    print(counter)
+                    loto = string
+            else:
+                counter += 1
+                if counter > counter1:
+                    print("too many guesses")
+                    break
+                else:
+                    print("Not closey")
+                    loto = string
+        else:
+            print("WINWINWINWIN")
+
 
     def __init__(self, master, counter=0, counter1=1, luckvar=3, countup=2, level=1):
         frame = Frame(master)
         frame.pack()
-
         luck = random.randint(1, luckvar)
+
 
         self.Label = Label(text="You are on LVL " + str(level) + "! To win EASY pick a number between 1 to " + str(luckvar) + " in " + str(countup) + " guesses", padx=20, pady=40)
         self.Label.pack(side=TOP, pady=40)
